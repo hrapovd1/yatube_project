1e65e242f12fc8aa3c22dae3662c71326b13ec06
Dmitry Khrapov <hrapovd@gmail.com>
2022-05-08 22:02:13 +0300

    worked version with db query posts and groups

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Post
+from .models import Group
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group', )
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-no-text-'
+    list_editable = ('group',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'title', 'description',)
+    list_filter = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from typing import Any
+from .models import Post
+from .models import Group
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.order_by('-pub_date')[:5]
     context = {
-        'index': 'Это главная страница проекта Yatube',
+        'posts': posts
     }
     template = 'posts/index.html'
     return render(request, template, context)
 
 
 def group_posts(request: HttpRequest, slug: Any) -> HttpResponse:
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'group_posts': 'Здесь будет информация о группах проекта Yatube',
-        'slug': slug,
+        'group': group,
+        'posts': posts,
     }
-    template = 'posts/group.html'
+    template = 'posts/group_list.html'
     return render(request, template, context)
